041aba8aeeb373efb3f1aceeab368f57f60aae0f
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2026-01-27 22:43:12 +0300

    init project learn

--- user.go
+++
@@ -1,9 +0,0 @@
-package lerangolang
-
-// структура элемента таблицы user
-type User struct {
-	Id       int    `json:"-"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
